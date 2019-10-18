4a70ca09581a429109e0c8b3eaa50b315d850417
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-10-18 07:16:36 -0600

    implemented validation for name, email, & password with error messages

--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -3,21 +3,28 @@ import axios from "axios";
 import { Form, Field, withFormik } from "formik";
 import * as Yup from "yup";
 
-const UserForm = () => {
-    // const [users, setUsers] = useState([]);
+const UserForm = ({ values, touched, errors }) => {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+
+    })
 
     return (
         <div className="user-form">
             <h1>User Onboarding</h1>
             <Form className="form">
                 <Field type="text" name="name" placeholder="Name" />
+                {touched.name && errors.name && (<p className="error">{errors.name}</p>)}
                 <Field type="text" name="email" placeholder="Email" />
+                {touched.email && errors.email && (<p className="error">{errors.email}</p>)}
                 <Field type="text" name="password" placeholder="Password" />
+                {touched.password && errors.password && (<p className="error">{errors.password}</p>)}
                 <label className="checkbox-container">
                     I agree to the Terms of Service
                     <Field
                         type="checkbox"
                         name="terms-of-service"
+                        checked={values.termsOfService}
                     />
                 </label>
                 <button type="submit">Submit</button>
@@ -26,12 +33,19 @@ const UserForm = () => {
     )
 }
 const FormikUserForm = withFormik({
-    mapPropsToValues({ name, email, password }) {
+    //sets the default values when the form loads
+    mapPropsToValues({ name, email, password, termsOfService }) {
         return {
             name: name || "",
             email: email || "",
-            password: password || ""
+            password: password || "",
+            termsOfService: termsOfService || false
         }
-    }
+    },
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required(),
+        email: Yup.string().required(),
+        password: Yup.string().required()
+    })
 })(UserForm);
 export default FormikUserForm;
